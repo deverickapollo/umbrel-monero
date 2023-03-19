d8fdf1bcfb28d9946c1e0eaa851ae4a9956440a6
Deverick <deverick@me.com>
2023-03-19 12:28:50 -0500

    Update RPC port to 18089

--- monero-entrypoint.sh
+++ monero-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/opt/homebrew/bin/bash bash
 
 CORE_PORT=18080
-CORE_RPCPORT=18081
+CORE_RPCPORT=18089
 
 arguments=""
 for env in "${!CORE_@}"
@@ -12,4 +12,4 @@ do
   arguments="${arguments} -${lowercase_flag,,}=${!env}"
 done
 echo $arguments
-# -port=18080 -rpcport=18081
+# -port=18080 -rpcport=18089
