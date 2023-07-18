4fb4ca53bd43e82ef3f2746792f5897aec951eae
Deverick <deverick@me.com>
2023-07-18 14:27:49 -0500

    Change color of address

--- ui/src/components/DonationModal.vue
+++ ui/src/components/DonationModal.vue
@@ -1,6 +1,6 @@
 <template v-slot:modal-footer>
   <div class="col-12 text-center">
-      <!-- XMR Donation Address: <code>86PKfN1ExxVe1FH2x7kTeuAtieZRAErbU1gV4qDkrpMKVNcfgAi8Dqz9oXhWCdCEcK6iqgxzdM6GZPCjVejAQ2dtQkWPyXv</code> -->
+      XMR Donation Address: <code class="donation-link">86PKfN1ExxVe1FH2x7kTeuAtieZRAErbU1gV4qDkrpMKVNcfgAi8Dqz9oXhWCdCEcK6iqgxzdM6GZPCjVejAQ2dtQkWPyXv</code>
   </div>
 </template>
 
