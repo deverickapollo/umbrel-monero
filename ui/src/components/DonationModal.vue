d2c2ada73347dfecda1386393e266d3d4a1de524
Deverick <deverick@me.com>
2023-07-18 11:48:02 -0500

    Add donation address

---
+++ ui/src/components/DonationModal.vue
@@ -0,0 +1,19 @@
+<template v-slot:modal-footer>
+  <div class="col-12 text-center">
+      <!-- XMR Donation Address: <code>86PKfN1ExxVe1FH2x7kTeuAtieZRAErbU1gV4qDkrpMKVNcfgAi8Dqz9oXhWCdCEcK6iqgxzdM6GZPCjVejAQ2dtQkWPyXv</code> -->
+  </div>
+</template>
+
+<style lang="scss">
+  @media (min-width: 576px) {
+    .w-sm-75 { 
+      width: 75% !important;
+    }
+  }
+</style>
+
+<script>
+  export default {
+    name: "DonationModal",
+  };
+</script>
