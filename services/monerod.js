d286871f11f8e64d47816b8778444b53ba1a7bd7
Deverick <deverick@me.com>
2023-09-05 21:05:30 -0500

    Update var name for user/pass

--- services/monerod.js
+++ services/monerod.js
@@ -1,10 +1,10 @@
 const RpcClient = require('monero-javascript');
 const MonerodError = require('models/errors.js').MonerodError;
 
-const MONEROD_RPC_PORT = process.env.RPC_PORT || 18081; // eslint-disable-line no-magic-numbers, max-len
+const MONEROD_RPC_PORT = process.env.MONERO_RPC_PORT || 18081; // eslint-disable-line no-magic-numbers, max-len
 const MONEROD_HOST = process.env.MONERO_HOST || '127.0.0.1';
-const MONEROD_RPC_USER = process.env.RPC_USER;
-const MONEROD_RPC_PASSWORD = process.env.RPC_PASSWORD;
+const MONEROD_RPC_USER = process.env.MONERO_RPC_USER || 'monero';
+const MONEROD_RPC_PASSWORD = process.env.MONERO_RPC_PASSWORD;
 
 const MONEROD_IP = `http://${MONEROD_HOST}:${MONEROD_RPC_PORT}`;
 
